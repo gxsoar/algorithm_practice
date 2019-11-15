037384ad64c5d1e5ffd8ebab675fc677192ac03d
gxsoar <2278926493gx@gmail.com>
2019-11-15 20:21:58 +0800

    update leetcode75 by C++ 2019/11/14

--- leetcode/leetcode_75/main1.cpp
+++ leetcode/leetcode_75/main1.cpp
@@ -5,10 +5,33 @@ using namespace std;
 class Solution {
    public:
     void sortColors(vector<int>& nums) {
-        
+        int p0 = 0, p2 = nums.size() - 1;
+        int now = 0;
+        while(now <= p2) {
+            if(nums[now] == 0) {
+                swap(nums[now++], nums[p0++]);
+            }
+            else if (nums[now] == 2) {
+                swap(nums[now++], nums[p2--]);
+            }
+            else now++;
+        }
     }
 };
 
 int main() {
+    Solution e;
+    vector<int> v;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int x; cin >> x;
+        v.push_back(x);
+    }
+    e.sortColors(v);
+    for(auto vh : v) {
+        cout << vh << " ";
+    }
+    cout << endl;
     return 0;
 }
