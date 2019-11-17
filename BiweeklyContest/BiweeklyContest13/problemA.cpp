8651017dbca980dab5d6bf13f0e19b76ba1fac68
gxsoar <2278926493gx@gmail.com>
2019-11-18 00:17:20 +0800

    update problemA

--- BiweeklyContest/BiweeklyContest13/problemA.cpp
+++ BiweeklyContest/BiweeklyContest13/problemA.cpp
@@ -5,7 +5,6 @@ using namespace std;
 class Solution {
    public:
     string encode(int num) {
-<<<<<<< HEAD
         string ans;
         num++;
         while (num) {
@@ -15,23 +14,9 @@ class Solution {
         ans.pop_back();
         reverse(ans.begin(), ans.end());
         return ans;
-=======
-        if(num == 0) return "";
-        if(num == 1) return "0";
-        if(num == 2) return "1";
-        
->>>>>>> 4932d38a9fe55a79233a312c4e3c6d21cec2e913
     }
 };
 
 int main() {
-<<<<<<< HEAD
-=======
-    int n;
-    cin >> n;
-    char s[1110];
-    itoa(n,s,2);
-    printf("%s\n",s);
->>>>>>> 4932d38a9fe55a79233a312c4e3c6d21cec2e913
     return 0;
 }
